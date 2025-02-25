75fe7c36da88c206f1ea5e68b009fac8a4733474
Hiddenleaf07 <162790981+Hiddenleaf07@users.noreply.github.com>
2025-02-25 13:15:20 +0530

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,12 +4,12 @@
 python3 -m pip install --upgrade pip
 
 # Install TA-Lib (corrected)
-wget https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz
-tar -xzf ta-lib-0.6.4-src.tar.gz
-cd ta-lib-0.6.4-src
+wget https://github.com/ta-lib/ta-lib/releases/download/v0.6.3/ta-lib-0.6.3-src.tar.gz
+tar -xzf ta-lib-0.6.3-src.tar.gz
+cd /workspaces/testing-new-py/ta-lib-0.6.3
 ./configure --prefix=/usr/local  # Important: Specify a prefix!
-make -j$(nproc)
-make install
+make
+sudo make install
 
 # Install TA-Lib's Python wrapper
 pip install TA-Lib
